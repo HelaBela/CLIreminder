426aab0f88fa6a1b53433547d5736edd206575ae
Helena Franczak <helena.franczak@myob.com>
2021-06-09 16:39:30 +1000

    first commit

---
+++ cmd/examples/cli-basics/os-args/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("no comand provided")
+		os.Exit(2)
+
+	}
+	//cmd := os.Args[1]
+
+	// switch cmd{
+
+	// }
+}
